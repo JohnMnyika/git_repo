c0d4eae34411cbfd8a76dc420a03bba859b5e88d
JohnMnyika <jmnyika514@gmail.com>
2017-06-15 22:07:12 +0300

    added a foreach loop

--- index.php
+++ index.php
@@ -4,7 +4,19 @@
     </head>
     <body>
     <?php
-        // going to do a for loop
+        // going to do a numeric array and loop through it's contents
+        $numbers = [
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        ];
+
+        // count the array to make sure that it has items
+        $how_many = count($numbers);
+
+        if($how_many > 0){
+            foreach ($numbers as $value){
+                echo '<pre>' . $value . '</pre><br/>';
+            }
+        }
     ?>
     </body>
 </html>
